ab65ae25a33b2802feaa34de4d39c9b4e7c9b533
s0619107 <s0619107@monmouth.edu>
2018-04-19 22:07:05 -0400

    FrequentGuest.java

--- Frequent/FrequentGuest.java
+++ Frequent/FrequentGuest.java
@@ -1,8 +1,38 @@
-import java.util.ArrayList;
 
 public class FrequentGuest {
 
-	private UserData[] ud;   
-    
-    
+	private String[][] ud;   
+	
+	public FrequentGuest(String[][] userData)
+	{
+		//constructor
+		for(int i=0; i<userData.length; i++)
+			  for(int j=0; j<userData[i].length; j++)
+			  ud[i][j] = userData[i][j];
+	}
+	
+
+	public void addGuestOrder(String[][] input)
+	{
+		int count;
+		count = ud[1].length + 1;
+		for(int i=count; i<input.length; i++)
+			  for(int j=0; j<input[i].length; j++)
+			    ud[i][j]=input[i][j];
+	}
+	
+	public String[][] getGuestOrder(String user)
+	{
+		String[][] temp = new String[1][3];
+		for(int i=0;i<ud.length;i++)
+		{
+			if(ud[i][1] == user)
+			{
+				temp[i][1] = ud[i][1];
+				temp[i][2] = ud[i][2];
+				temp[i][3] = ud[i][3];
+			}
+		}
+	return temp;
+	}
 }
